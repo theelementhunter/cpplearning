965ab2e2913bda615bef8f6d4256c5a62512b852
thelementhunter <acqviolinist@gmail.com>
2026-02-14 14:10:36 -0500

    main.cpp - test_function for SmartIntArray

--- main.cpp
+++ main.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
-#include "SmartIntArray.h" // Include your new header
+#include <cmath>
+#include "SmartIntArray.h"
+
+void test_function() {
+    std::cout << "#--- Inside Function ---#" << std::endl;
+
+    // Array Size
+    int array_size = 7;
+
+    // Build Specific instance of SmartIntArray Class
+    SmartIntArray array_1(array_size);
+
+    // Manually putting things in the array
+    array_1[0] = 1;
+    array_1[1] = 2;
+
+
+    // Looping to fill the array and print its values
+    for (int i = 0; i < array_size; i++) {
+        array_1[i] = static_cast<int>(std::pow(i, 2));
+        std::cout << "Inside index " << i << ": "<< array_1[i] << std::endl;
+    }
+
+    //Using our helper function
+    std::cout << array_1.getSize() << " <-- Array Size" << std::endl;
+
+    // Destructor Activates as soon as the ending bracket comes
+    std::cout << "#--- Destructor is destructing ---#" << std::endl;
+
+    //
+}
 
 int main() {
-    std::cout << "Hello, Smart Array!" << std::endl;
-
-    // We will test your class here later
-    // SmartIntArray arr(5);
-
+    test_function();
+    std::cout << "Back in main." << std::endl;
     return 0;
 }
